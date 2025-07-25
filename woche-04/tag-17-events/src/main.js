8f6bbbd873abfb44e7a6d1bd2ec501afb2286d0f
AbuTokio <bunyaminxkara@gmail.com>
2025-07-25 11:14:59 +0200

    Tag 17 - Events - js done

--- woche-04/tag-17-events/src/main.js
+++ woche-04/tag-17-events/src/main.js
@@ -28,3 +28,65 @@ document.querySelector("#login").addEventListener("submit", function (event) {
     result.innerText = "Username oder Password ist falsch."
   }
 })
+
+// ~ Beispiel 2
+
+document.querySelector("#live-input").addEventListener("input", function (event) {
+  const inputValue = event.target.value
+  console.log("🚀 ~ inputValue:", inputValue.toUpperCase())
+  document.querySelector("#live-input").style.color = "red"
+})
+
+document.querySelector("#agree-checkbox").addEventListener("change", function (e) {
+  const checkBoxValue = e.target.checked
+  console.log("🚀 ~ checkBoxValue:", checkBoxValue)
+})
+
+// ~ Beispiel 3
+
+document.querySelector("#color-picker").addEventListener("input", function (event) {
+  const inputElementValue = event.target.value
+  document.body.style.backgroundColor = inputElementValue
+})
+
+// ~ Beispiel 4
+
+const myBtn = document.querySelector("#say-hi-btn")
+const helloOutputElement = document.querySelector("#hello-output")
+
+myBtn.addEventListener("click", function () {
+  helloOutputElement.innerText = "Hallooo vom AddEventListener"
+})
+
+// ~ Beispiel 5
+
+const textInputElement = document.querySelector("#text-input")
+
+textInputElement.addEventListener("mouseover", function () {
+  textInputElement.style.border = "2px solid red"
+  console.log("Die Maus wurde über mir bewegt")
+})
+
+textInputElement.addEventListener("mouseout", function () {
+  textInputElement.style.border = "2px solid blue"
+})
+
+// ~ Beispiel 6
+
+const selectInputElement = document.querySelector("#select-input")
+const selectOutputElement = document.querySelector("#select-output")
+
+selectInputElement.addEventListener("change", function (e) {
+  console.log("🚀 ~ e.target.value:", e.target.value)
+  const selectInputValue = selectInputElement.value
+  selectOutputElement.innerText = `Your Order is ${selectInputValue}`
+})
+
+// ~ Beispiel 7
+
+const hacker = document.querySelector("#hacker")
+const log = document.querySelector("#log")
+
+hacker.addEventListener("keyup", function (event) {
+  console.log("🚀 ~ event.code:", event.code)
+})
