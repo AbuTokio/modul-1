3294ce101b9ced6a179505f1c1dd099795b04dd3
AbuTokio <bunyaminxkara@gmail.com>
2025-07-25 11:59:23 +0200

    Tag 17 - TypeScript Intro

---
+++ woche-04/tag-17-ts-ohne-vite/src/index.js
@@ -0,0 +1,18 @@
+"use strict";
+// Primitive Typen in der Programmiersprache
+let firstName = "max";
+let age = 22;
+let isOnline = true;
+function sayHello(name) {
+    return "Hello, " + name;
+}
+console.log(sayHello("Andre"));
+console.log(sayHello("Joe"));
+function logMessage(message) {
+    console.log("Log:", message);
+}
+logMessage("Heute ist ein schöner Tag.");
+function calcArea(width, height) {
+    return width * height;
+}
+console.log(calcArea(100, 20));
